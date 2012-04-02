5ea3991d4c56a8ad64e8582a3e6c2b8252bb47ba
Rishi Narang <rishinarang15@gmail.com>
2012-04-02 18:17:20 -0400

    another attempt at fixing the server

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-#from MySQLdb import connect
-#from MySQLdb import cursors
+from MySQLdb import connect
+from MySQLdb import cursors
 
 from flask import Flask
 app = Flask(__name__)
@@ -30,8 +30,8 @@ def eventsList():
     return table
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    #app.debug = True
+    #app.run()
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
