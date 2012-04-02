ea62fcfdfa8ae0e82e8d70ced4e071a2fbbc8796
Rishi Narang <rishinarang15@gmail.com>
2012-04-01 21:04:06 -0400

    test change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!\n\n\tLove, Rishi'
+    return 'Hi Yeonjoo!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
