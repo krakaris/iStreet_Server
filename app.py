07f9c546944ad7119b4a665612207ee9a66be3bb
Rishi Narang <rishinarang15@gmail.com>
2012-04-05 00:23:24 -0400

    accidentally pushed wrong code last time (same message follows): basic server setup with events list, specific event info, and event listings for a specific club. code should be fairly clean. TODO: have the code automatically identify if it's on the server.

--- app.py
+++ app.py
@@ -1,40 +1,67 @@
-import os
 from MySQLdb import connect
 from MySQLdb import cursors
 
-from flask import Flask
+import json
+
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Use: /name'
+    return 'Uses: /eventslist, /eventinfo, /clubevents'
 
-@app.route('/<name>')
-def hello(name):
-    return 'param: %s' % name
-
-@app.route('/eventslist')
+@app.route('/eventslist', methods = ['GET'])
 def eventsList():
+    return getJSONForQuery("select title, event_id, name from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) > '2012-03-01'")
+
+@app.route('/eventinfo', methods = ['GET'])
+def eventInfo():
+    event_id = request.args.get("event_id")
+    if event_id == None:
+        return "Invalid request: missing event_id argument"
+
+    return getJSONForQuery("select title, event_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and event_id = " + str(event_id))
+
+@app.route('/clubevents', methods = ['GET'])
+def clubEvents():
+    name = request.args.get("name")
+    if name == None:
+        return "Invalid request: missing name argument"
+
+    #cursor = sendQuery("select title, event_id, pam_event.club_id, pam_club.club_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and name = \"" + name + "\" and DATE(time_start) = '2012-03-10'")
+    return getJSONForQuery("select title, event_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and name = \"" + name + "\"")
+
+def getJSONForQuery(query):
+    cursor = sendQuery(query)
+    table = getDictForQueryResults(cursor)
+    return str(json.dumps(table, encoding = "latin-1"))
+
+def sendQuery(query):
     host = 'dev.tigerapps.org'
     user = 'iossvr2984'
     passwd = '5TGCj3fN4E'
     db = 'tigerapps'
+    
     connection = connect(host = host, user = user, passwd = passwd, db = db)
-
-    # Create cursor as a dictionary:
     cursor = connection.cursor(cursors.DictCursor)
-    cursor.execute("select * from pam_event")
-    row = cursor.fetchone()
-    table = ""
-    while row:
-        table = table + row["title"] + ", "
-        row = cursor.fetchone()
+    cursor.execute(query)
+    
+    return cursor
 
-    return table
+def getDictForQueryResults(cursor):
+    results = []
+    row = cursor.fetchone()
+    while row:
+        for key in row:
+            row[key] = str(row[key]) 
+            '''necessary for things like datetime objects'''
+        results.append(row)
+        row = cursor.fetchone()
+    return results
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    #app.debug = True
+    #app.run()
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
