c2d8eb4c90195918932a8ece246e75bf4e9af43b
Rishi Narang <rishinarang15@gmail.com>
2012-04-05 00:21:03 -0400

    basic server setup with events list, specific event info, and event listings for a specific club. code should be fairly clean. TODO: have the code automatically identify if it's on the server.

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def eventsList():
     return table
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    #app.debug = True
+    #app.run()
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
