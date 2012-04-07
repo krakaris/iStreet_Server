102f3e13f7553a78fab30999aff29f96f9a56f41
Alexa Krakaris <krakaris@gmail.com>
2012-04-07 14:01:06 -0400

    updated to prod server and date

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/eventslist', methods = ['GET'])
 def eventsList():
-    return getJSONForQuery("select title, event_id, poster, name from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) > '2012-03-01'")
+    return getJSONForQuery("select title, event_id, poster, name from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE()")
 
 @app.route('/eventinfo', methods = ['GET'])
 def eventInfo():
@@ -30,7 +30,7 @@ def clubEvents():
         return "Invalid request: missing name argument"
 
     #cursor = sendQuery("select title, event_id, pam_event.club_id, pam_club.club_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and name = \"" + name + "\" and DATE(time_start) = '2012-03-10'")
-    return getJSONForQuery("select title, event_id, poster, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and name = \"" + name + "\"")
+    return getJSONForQuery("select title, event_id, poster, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and time_start <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() and name = \"" + name + "\"")
 
 def getJSONForQuery(query):
     cursor = sendQuery(query)
@@ -38,9 +38,9 @@ def getJSONForQuery(query):
     return str(json.dumps(table, encoding = "latin-1"))
 
 def sendQuery(query):
-    host = 'dev.tigerapps.org'
-    user = 'iossvr2984'
-    passwd = '5TGCj3fN4E'
+    host = 'www.tigerapps.org'
+    user = 'iossvr24093'
+    passwd = 'FpmMQe9MQRXgO63'
     db = 'tigerapps'
     
     connection = connect(host = host, user = user, passwd = passwd, db = db)
