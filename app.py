f5077cbd2d6ca9ed11e89c739c17173a1a00b6c0
Alexa Krakaris <krakaris@gmail.com>
2012-04-07 14:03:54 -0400

    order by date

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/eventslist', methods = ['GET'])
 def eventsList():
-    return getJSONForQuery("select title, event_id, poster, name from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE()")
+    return getJSONForQuery("select title, event_id, poster, name, time_start from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() ORDER BY time_start")
 
 @app.route('/eventinfo', methods = ['GET'])
 def eventInfo():
@@ -30,7 +30,7 @@ def clubEvents():
         return "Invalid request: missing name argument"
 
     #cursor = sendQuery("select title, event_id, pam_event.club_id, pam_club.club_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and name = \"" + name + "\" and DATE(time_start) = '2012-03-10'")
-    return getJSONForQuery("select title, event_id, poster, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and time_start <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() and name = \"" + name + "\"")
+    return getJSONForQuery("select title, event_id, poster, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and time_start <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() and name = \"" + name + "\" ORDER BY time_start")
 
 def getJSONForQuery(query):
     cursor = sendQuery(query)
