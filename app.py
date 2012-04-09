869507176825dc6c70ed39d068d49eb4bcb74f08
Rishi Narang <rishinarang15@gmail.com>
2012-04-09 14:45:23 -0400

    debug/release switch

--- app.py
+++ app.py
@@ -81,8 +81,8 @@ def get_messages():
     return get_messages(past)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    #app.debug = True
+    #app.run()
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
