16bfab8dc66f15e27259074d2a594d7fa21cce54
Rishi Narang <rishinarang15@gmail.com>
2012-04-12 20:09:30 -0400

    updated chat.py to get the last 50 messages, but in descending (reversed) order

--- chat.py
+++ chat.py
@@ -11,7 +11,7 @@ def add(user_id, message):
 
 '''returns JSON string with id, added (timestamp), user_id, and message'''
 def get(past):
-    if (past != ""):
-        return app.getJSONForQuery("SELECT * FROM chatitems WHERE id > " + escape_string(past) + " ORDER BY added LIMIT 50", "istreet")
-    else:
-        return app.getJSONForQuery("SELECT * FROM chatitems ORDER BY added LIMIT 50", "istreet")
+    
+    if (past == ""):
+        past = "-1"
+    return app.getJSONForQuery("SELECT * FROM chatitems WHERE id > " + escape_string(past) + " ORDER BY added DESC LIMIT 50", "istreet")
