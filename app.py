0ef1e17313b249b56c3e39f123e1bc859bf6e9bc
Rishi Narang <rishinarang15@gmail.com>
2012-04-25 20:57:38 -0400

    done withth many of the friends server calls, still need comments and security checks, and need to fix getUsersForEvent

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def clubsList():
 
 def getJSONForQuery(query, database):
     cursor = sendQuery(query, database)
-    table = getDictForQueryResults(cursor)
+    table = getDictArrayForQueryResults(cursor)
     return str(json.dumps(table, encoding = "latin-1"))
 
 def sendQuery(query, database):
@@ -68,7 +68,7 @@ def sendQuery(query, database):
     
     return cursor
 
-def getDictForQueryResults(cursor):
+def getDictArrayForQueryResults(cursor):
     results = []
     row = cursor.fetchone()
     while row:
@@ -99,68 +99,149 @@ def get_messages():
 @app.route('/updateUser', methods = ['POST'])
 def updateUser():
     # GET
-    netid = request.args.get("netid") #mandatory
+    fb_id = request.args.get("fb_id") #mandatory
+    if fb_id == None:
+        return "ERROR: missing fb_id parameter (HTTP GET)"
+    
     # POST
-    fbid = ""
-    if(request.form.has_key("fb_id")):
-           fbid = request.form["fb_id"]
     name = ""
     if(request.form.has_key("name")):
-       name = request.form["name"]
+        name = request.form["name"]
     events = ""
     if(request.form.has_key("events")):
         events = request.form["events"]
+        
+    userCursor = sendQuery("SELECT * FROM user WHERE fb_id = \'" + fb_id + "\'", "istreet")
+    theUser = userCursor.fetchone()
+    
+    if userCursor.fetchone():
+        sys.stderr.write("ERROR: inconsistent server state: >1 user with given fb_id")
+        return "ERROR: inconsistent server state - see error logs"
+    
+    if theUser:
+        # set once for each of the three, if not ""
+        if name != "":
+            sendQuery("UPDATE user SET name = \'" + name + "\' WHERE fb_id = \'" + fb_id + "\'", "istreet")
+        if events != "":
+            sendQuery("UPDATE user SET events = \'" + events + "\' WHERE fb_id = \'" + fb_id + "\'", "istreet")
+    else:
+        # insert a new row with name, events, fbid, eventslist
+        sendQuery(str.format("INSERT INTO user (name, events, fb_id) VALUES(\'{0}\', \'{1}\', \'{2}\')", name, events, fb_id), "istreet")
 
-    query = ""
-    # send query to see if user with netid already exist
-    # if the user already exists, update that row with the new information)
-    # if not, create a new wrote with that information
-    return ""
+    return "SUCCESS"
 
 @app.route('/attendEvent', methods = ['POST'])
 def attendEvent():
     # GET
-    netid = request.args.get("netid")
+    fb_id = request.args.get("fb_id")
+    if fb_id == None:
+        return "ERROR: missing fb_id parameter (HTTP GET)"
+    
     # POST
     event_id = ""
     if(request.form.has_key("event_id")):
         event_id = request.form["event_id"]
+        #TO DO: VALIDATE EVENT_ID
+    else:
+        return "ERROR: missing event_id parameter (HTTP POST)"
+        
+    userCursor = sendQuery("SELECT * FROM user WHERE fb_id = \'" + fb_id + "\'", "istreet")
+    theUser = userCursor.fetchone()
     
+    if not theUser:
+        return "ERROR: no user with fb_id \'" + fb_id + "\' exists"
+
+    if userCursor.fetchone():
+        sys.stderr.write("ERROR: inconsistent server state: >1 user with given fb_id")
+        return "ERROR: inconsistent server state - see error logs"    
+    
+    userEvents = theUser["events"]
+    eventsArray = userEvents.split(", ")
+    if not (event_id in eventsArray):
+        eventsArray.append(event_id)
+    newUserEvents = ", ".join(eventsArray)
+    sendQuery(str.format("UPDATE user SET events = \'{0}\' WHERE fb_id = \'{1}\'", newUserEvents, fb_id), "istreet") 
+
     # send query to get row for user
     # extract events list and append event to events list
     # send query to update the row for that user with the new events list
-    return ""
+    return "SUCCESS"
 
 @app.route('/unattendEvent', methods = ['POST'])
 def unattendEvent():
     # GET
-    netid = request.args.get("netid")
+    fb_id = request.args.get("fb_id")
+    if fb_id == None:
+        return "ERROR: missing fb_id parameter (HTTP GET)"
+    
     # POST
     event_id = ""
     if(request.form.has_key("event_id")):
         event_id = request.form["event_id"]
+        #TO DO: VALIDATE EVENT_ID
+    else:
+        return "ERROR: missing event_id parameter (HTTP POST)"
+        
+    userCursor = sendQuery("SELECT * FROM user WHERE fb_id = \'" + fb_id + "\'", "istreet")
+    theUser = userCursor.fetchone()
     
+    if not theUser:
+        return "ERROR: no user with fb_id \'" + fb_id + "\' exists"
+
+    if userCursor.fetchone():
+        sys.stderr.write("ERROR: inconsistent server state: >1 user with given fb_id")
+        return "ERROR: inconsistent server state - see error logs"    
+    
+    userEvents = theUser["events"]
+    eventsArray = userEvents.split(", ")
+    if (event_id in eventsArray):
+        eventsArray.remove(event_id)
+    
+    newUserEvents = ", ".join(eventsArray)
+    sendQuery(str.format("UPDATE user SET events = \'{0}\' WHERE fb_id = \'{1}\'", newUserEvents, fb_id), "istreet") 
+
     # send query to get row for user
     # extract events list and remove event from events list
     # send query to update the row for that user with the new events list
-    return ""
+    return "SUCCESS"
 
 @app.route('/getUsersForEvent', methods = ['GET'])
 def getUsersForEvent():
     # GET
     event_id = request.args.get("event_id")
-
-    # send query for list of fb_id's that are attending that event
-    # this one is probably the most complex; we'll have to see if there's some way of going through each user and seeing if their my_events contains that event. maybe we can do this one together.
-    return ""
+    if event_id == None:
+        return "ERROR: missing event_id parameter (HTTP GET)"
+    query = str.format("SELECT fb_id FROM user WHERE MATCH(events) AGAINST ('{0}' IN BOOLEAN MODE)", event_id)
+    print query
+    cursor = sendQuery(query, "istreet")
+    
+    row = cursor.fetchone()
+    fb_ids = []
+    while row:
+        print "row!"
+        fb_ids.append(row["fb_id"])
+        row = cursor.fetchone()
+    
+    return ", ".join(fb_ids)
 
 @app.route('/getEventsForUser', methods = ['GET'])
 def getEventsForUser():
     # GET
     fb_id = request.args.get("fb_id")
+    if fb_id == None:
+        return "ERROR: missing fb_id parameter (HTTP GET)"
+    
+    userCursor = sendQuery("SELECT * FROM user WHERE fb_id = \'" + fb_id + "\'", "istreet")
+    theUser = userCursor.fetchone()
+    
+    if not theUser:
+        return "ERROR: no user with fb_id \'" + fb_id + "\' exists"
 
-    # send query for list of event_id's for that user
-    return ""
+    if userCursor.fetchone():
+        sys.stderr.write("ERROR: inconsistent server state: >1 user with given fb_id")
+        return "ERROR: inconsistent server state - see error logs"    
+    
+    return theUser["events"]
 
 if __name__ == '__main__':
     if(len(sys.argv) > 1 and str.lower(sys.argv[1]) == "debug"):
