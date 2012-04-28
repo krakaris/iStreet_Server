b835cc69060ffacf7a8b2359ed421fb560a552cf
Rishi Narang <rishinarang15@gmail.com>
2012-04-27 22:24:03 -0400

    Fully secure servergit add CASClient.py app.py templates/test.html hooray

---
+++ CASClient.py
@@ -0,0 +1,36 @@
+import sys, os, cgi, urllib, re
+from flask import Flask, request, render_template, make_response, redirect
+
+class CASClient:
+
+    def __init__(self, url='https://fed.princeton.edu/cas/'):
+        self.cas_url = url
+
+    def Authenticate(self, ticket):
+        # If the request contains a login ticket, try to validate it
+        if ticket != "":
+            netid = self.Validate(ticket)
+            if netid != None:
+                return netid
+        # No valid ticket; redirect the browser to the login page to get one
+        login_url = self.cas_url + 'login' + '?service=' + self.ServiceURL()
+        response = redirect(login_url, code=307)
+        return response
+
+    def Validate(self, ticket):
+        val_url = self.cas_url + "validate" + \
+            '?service=' + urllib.quote(self.ServiceURL()) + \
+            '&ticket=' + urllib.quote(ticket)
+        r = urllib.urlopen(val_url).readlines()    # returns 2 lines
+        if len(r) == 2 and re.match("yes", r[0]) != None:
+            return r[1].strip()
+        return None
+
+    def ServiceURL(self):
+        return "http://localhost:5000/login"
+        
+def main():
+    print "CASClient does not run standalone"
+
+if __name__ == '__main__':
+    main()
--- app.py
+++ app.py
@@ -2,19 +2,73 @@
 import os
 from MySQLdb import connect
 from MySQLdb import cursors
+import CASClient
 
-import json, chat, string, sys
-
-from flask import Flask, request, render_template
+import json, chat, sys
+#import string
+import datetime
+from flask import Flask, request, render_template, redirect, session
 app = Flask(__name__)
+
+#used for sessions
+app.secret_key = "J\"JOx[Lq\'.jt7P.qT5i\'"
+#app.permanent_session_lifetime = datetime.timedelta(7)
+app.permanent_session_lifetime = 30
+app.session_cookie_name = "ISTREET_SESSION"
+app.session_cookie_domain = "istreetsvr.heroku.com"
 
 @app.route('/')
 def index():
-    return render_template('test.html')
-    #return 'Uses: /eventslist, /eventinfo, /clubevents'
+    netid = ""
+    if 'username' in session:
+        netid = session['username']
+    return render_template('test.html', netid=netid)
+    
+# returns netid or redirect object
+def authenticate():
+    if 'username' in session:
+        print "cookied in!"
+        return str(session['username'])
+    else:
+        return CASLogin()
+
+# returns netid or redirect object
+def CASLogin():
+    ticket = ""
+    if(request.args.has_key("ticket")):
+        ticket = request.args.get("ticket")
+    C = CASClient.CASClient()
+    response = C.Authenticate(ticket)
+    if(type(response) != str):
+        return response #redirect
+    else:
+        # create / confirm the existence of a cookie
+        session['username'] = response
+        session.permanent = True
+        return response
+
+@app.route('/login', methods = ['GET'])
+def login():
+    response = authenticate()
+    if(type(response) != str):
+        return response
+    else:
+        return "SUCCESS: " + response
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return "SUCCESS"
 
 @app.route('/eventslist', methods = ['GET'])
 def eventsList():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
     return getJSONForQuery("select title, event_id, poster, name, time_start, time_end, description, entry, entry_description from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) > '2012-03-30'", "tigerapps")
 #    return getJSONForQuery("select title, event_id, poster, name, time_start from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() ORDER BY time_start", "tigerapps")
 
@@ -30,6 +84,13 @@ def eventInfo():
 
 @app.route('/clubevents', methods = ['GET'])
 def clubEvents():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     name = request.args.get("name")
     if name == None:
         return "Invalid request: missing name argument"
@@ -40,6 +101,13 @@ def clubEvents():
 
 @app.route('/clubslist', methods = ['GET'])
 def clubsList():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     return getJSONForQuery("select club_id, name from pam_club", "tigerapps")
 
 def getJSONForQuery(query, database):
@@ -81,7 +149,13 @@ def getDictArrayForQueryResults(cursor):
 
 @app.route('/add', methods = ['POST'])
 def add_message():
-
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+    
     #TODO: what if the following two args dont exist?
     user_id = request.form['user_id']
     message = request.form['message']
@@ -91,6 +165,13 @@ def add_message():
 
 @app.route('/get', methods = ['GET'])
 def get_messages():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     past = request.args.get("past")
     if past == None:
         past = ""
@@ -98,6 +179,13 @@ def get_messages():
 
 @app.route('/updateUser', methods = ['POST'])
 def updateUser():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     # GET
     fb_id = request.args.get("fb_id") #mandatory
     if fb_id == None:
@@ -132,6 +220,13 @@ def updateUser():
 
 @app.route('/attendEvent', methods = ['POST'])
 def attendEvent():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     # GET
     fb_id = request.args.get("fb_id")
     if fb_id == None:
@@ -169,6 +264,13 @@ def attendEvent():
 
 @app.route('/unattendEvent', methods = ['POST'])
 def unattendEvent():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     # GET
     fb_id = request.args.get("fb_id")
     if fb_id == None:
@@ -207,6 +309,13 @@ def unattendEvent():
 
 @app.route('/getUsersForEvent', methods = ['GET'])
 def getUsersForEvent():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     # GET
     event_id = request.args.get("event_id")
     if event_id == None:
@@ -226,6 +335,13 @@ def getUsersForEvent():
 
 @app.route('/getEventsForUser', methods = ['GET'])
 def getEventsForUser():
+    netid = ""
+    response = authenticate()
+    if(type(response) != str):
+        return response #redirect
+    else:
+        netid = response
+        
     # GET
     fb_id = request.args.get("fb_id")
     if fb_id == None:
