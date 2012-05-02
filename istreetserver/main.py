0db36aff7ccb9debf96a281377aee64ddeaff22e
Rishi Narang <rishinarang15@gmail.com>
2012-05-02 02:47:02 -0400

    added eventinfo back

--- istreetserver/main.py
+++ istreetserver/main.py
@@ -18,15 +18,15 @@ def eventsList(netid):
     return getJSONForQuery("select title, event_id, poster, name, time_start, time_end, description, entry, entry_description from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) > '2012-03-30'", "tigerapps")
 #    return getJSONForQuery("select title, event_id, poster, name, time_start from pam_event, pam_club WHERE pam_club.club_id = pam_event.club_id AND DATE(time_start) <= DATE_ADD(CURDATE(), INTERVAL 7 DAY) and time_start >= CURDATE() ORDER BY time_start", "tigerapps")
 
-'''
+
 @app.route('/eventinfo', methods = ['GET'])
+@requires_CASauth
 def eventInfo():
     event_id = request.args.get("event_id")
     if event_id == None:
         return "Invalid request: missing event_id argument"
 
     return getJSONForQuery("select title, poster, event_id, time_start, time_end, description, entry, entry_description, name from pam_event, pam_club where pam_event.club_id = pam_club.club_id and event_id = " + str(event_id), "tigerapps")
-'''
 
 @app.route('/clubevents', methods = ['GET'])
 @requires_CASauth
